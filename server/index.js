0c835f2a4bb36c7173531f8dd6c86aa9182cda99
Moises Hernandez <moisesher18@gmail.co>
2018-09-12 22:00:05 -0500

    gb

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use('/api/cycles',require('./routes/cycles.routes'));
 // Create link to Angular build directory
 //var distDir = __dirname + "/dist/";
 //app.use(express.static(distDir));
-app.use("/*",express.static(__dirname + "/dist/"));
+app.use("/*",express.static(__dirname + "/dist/")); 
 
 
 //OMW server
