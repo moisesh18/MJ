8f23a4ac688249753713c59f03b143cf1de37d27
EuniLG <31998921+EuniLG@users.noreply.github.com>
2019-04-05 13:55:44 -0600

    DATABASE

--- server/database.js
+++ server/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 //en la um
-//const URI = 'mongodb://172.16.174.214/mj';
+const URI = 'mongodb://172.16.174.214/mj';
 //localhost
-const URI = 'mongodb://localhost/mj';
+//const URI = 'mongodb://localhost/mj';    
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('Base de datos conectada: ' + URI))
     .catch(err => console.error("Imposible conectar a " + URI));
