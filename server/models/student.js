64e6407a7ef17739fec4e23b80d1563d266a520c
Moises Hernandez <moisesher18@gmail.co>
2019-04-10 15:35:48 -0500

    Database ignored

--- server/models/student.js
+++ server/models/student.js
@@ -28,7 +28,7 @@ const StudentsSchema = new Schema({
 
 StudentsSchema.virtual('fullName').get(function () {
     return this.first_name + ' ' + this.last_name;
-});
+}); 
 
 StudentsSchema.virtual('student').get(function () {
     return this._id + ' - ' + this.first_name + ' ' + this.last_name;
