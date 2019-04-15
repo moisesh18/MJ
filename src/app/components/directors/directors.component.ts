a034ae5b5a30e55256484e728a3e462703926629
Moises Hernandez <moisesher18@gmail.co>
2019-04-15 14:42:13 -0500

    Permisos

--- src/app/components/directors/directors.component.ts
+++ src/app/components/directors/directors.component.ts
@@ -9,155 +9,159 @@ import { NgForm } from '@angular/forms';
 declare var $: any;
 
 @Component({
-    selector: 'app-directors',
-    templateUrl: './directors.component.html',
-    styleUrls: ['./directors.component.css'],
-    providers: [DirectorsService]
+  selector: 'app-directors',
+  templateUrl: './directors.component.html',
+  styleUrls: ['./directors.component.css'],
+  providers: [DirectorsService]
 })
 export class DirectorsComponent implements OnInit {
-    select_student: any;
-    select_club: any;
-    columns: {};
-    constructor(public AuthService: AuthService, public service: DirectorsService) {
-        var self = this;
-        var operateEvents = {
-            'click .edit': function (e, value, row, index) {
-                self.select(row)
-            },
-            'click .delete': function (e, value, row, index) {
-                self.delete(row._id)
-            }
-        }
-        this.columns = [
-            [{
-                title: 'Puesto',
-                field: 'role',
-                sortable: true
-            },
-            {
-                title: 'Matricula',
-                field: 'student._id',
-                sortable: true
-            },
-            {
-                title: 'Nombre',
-                field: 'student.fullName',
-                sortable: true
-            },
-            {
-                title: 'Club',
-                field: 'club.name',
-                sortable: true
-            }, {
-                field: 'operate',
-                title: 'Operaciones',
-                align: 'center',
-                events: operateEvents,
-                formatter: this.operateFormatter()
-            }]
-        ];
+  select_student: any;
+  select_club: any;
+  columns: {};
+  constructor(public AuthService: AuthService, public service: DirectorsService) {
+    var self = this;
+    var operateEvents = {
+      'click .edit': function (e, value, row, index) {
+        self.select(row)
+      },
+      'click .delete': function (e, value, row, index) {
+        self.delete(row._id)
+      }
+    }
+    this.columns = [
+      [{
+        title: 'Puesto',
+        field: 'role',
+        sortable: true
+      },
+      {
+        title: 'Matricula',
+        field: 'student._id',
+        sortable: true
+      },
+      {
+        title: 'Nombre',
+        field: 'student.fullName',
+        sortable: true
+      },
+      {
+        title: 'Club',
+        field: 'club.name',
+        sortable: true
+      }, {
+        field: 'operate',
+        title: 'Operaciones',
+        align: 'center',
+        events: operateEvents,
+        formatter: this.operateFormatter()
+      }]
+    ];
+  }
+
+  async ngOnInit() {
+    this.get();
+    this.getSelect();
+    $('.bTable').bootstrapTable({
+      columns: this.columns,
+      showExport: true,
+      exportDataType: 'all',
+      exportTypes: ['excel'],
+      search: true,
+      sortName: "student.fullName",
+      sortOrder: "asc",
+      pagination: true,
+      showPaginationSwitch: true,
+      rememberOrder: true,
+      showColumns: true,
+      locale: "es-MX",
+      exportOptions: {
+        "fileName": "Directiva",
+        "ignoreColumn": ["operate"]
+      }
+    })
+  }
+
+  get() {
+    this.service.get()
+      .subscribe(res => {
+        var data = res as Director[]
+        $('.bTable').bootstrapTable("load", data)
+      });
+  }
+
+  operateFormatter() {
+    if (this.AuthService.gisAdmin) {
+      return [
+        '<a href="javascript:void(0)" class="edit" *ngIf="false"><i class="material-icons">edit</i></a><a href="javascript:void(0)" class="delete"><i class="material-icons">delete</i></a>'
+      ].join('')
+    } else {
+      return [''].join('')
+    }
+  }
+
+  add(form?: NgForm) {
+    if (form.value.password != "" && form.value.password != form.value.password2) {
+      alert('Las contraseñas no son iguales');
+      return false;
+    }
+    if (form.value._id) {
+      if (form.value.password == "") {
+        delete form.value.password;
+      }
+      this.service.put(form.value)
+        .subscribe((res: any) => {
+          this.AuthService.toast(res.message)
+          if (res.success) {
+            this.resetForm(form);
+            this.get();
+          }
+        });
+    } else {
+      delete form.value._id;
+      this.service.post(form.value)
+        .subscribe((res: any) => {
+          this.AuthService.toast(res.message)
+          if (res.success) {
+            this.resetForm(form);
+            this.get();
+          }
+        });
     }
 
-    async ngOnInit() {
-        this.get();
-        this.getSelect();
-        $('.bTable').bootstrapTable({
-            columns: this.columns,
-            showExport: true,
-            exportDataType: 'all',
-            exportTypes: ['excel'],
-            search: true,
-            sortName: "student.fullName",
-            sortOrder: "asc",
-            pagination: true,
-            showPaginationSwitch: true,
-            rememberOrder: true,
-            showColumns: true,
-            locale: "es-MX",
-            exportOptions: {
-                "fileName": "Directiva",
-                "ignoreColumn": ["operate"]
-            }
-        })
-    }
+  }
 
-    get() {
-        this.service.get()
-            .subscribe(res => {
-                var data = res as Director[]
-                $('.bTable').bootstrapTable("load", data)
-            });
-    }
+  select(single: Director) {
+    single.password = "";
+    this.service.selected = single;
+  }
 
-    operateFormatter() {
-        return [
-            '<a href="javascript:void(0)" class="edit" *ngIf="false"><i class="material-icons">edit</i></a><a href="javascript:void(0)" class="delete"><i class="material-icons">delete</i></a>'
-        ].join('')
-    }
+  getSelect() {
+    this.service.getStudents()
+      .subscribe(res => {
+        this.select_student = res as Student[];
+      });
+    this.service.getClubs()
+      .subscribe(res => {
+        this.select_club = res as Club[];
+      });
+  }
 
-    add(form?: NgForm) {
-        if (form.value.password != "" && form.value.password != form.value.password2) {
-            alert('Las contraseñas no son iguales');
-            return false;
-        }
-        if (form.value._id) {
-            if (form.value.password == "") {
-                delete form.value.password;
-            }
-            this.service.put(form.value)
-                .subscribe((res: any) => {
-                    this.AuthService.toast(res.message)
-                    if (res.success) {
-                        this.resetForm(form);
-                        this.get();
-                    }
-                });
-        } else {
-            delete form.value._id;
-            this.service.post(form.value)
-                .subscribe((res: any) => {
-                    this.AuthService.toast(res.message)
-                    if (res.success) {
-                        this.resetForm(form);
-                        this.get();
-                    }
-                });
-        }
-
+  delete(_id: string) {
+    if (confirm('¿Estas seguro de eliminar este usuario?')) {
+      this.service.delete(_id)
+        .subscribe((res: any) => {
+          this.get();
+          this.AuthService.toast(res.message)
+        });
     }
+  }
 
-    select(single: Director) {
-        single.password = "";
-        this.service.selected = single;
+  resetForm(form?: NgForm) {
+    if (form) {
+      form.reset();
+      $('#studentForm #edit').hide();
+      $('#studentForm #submit').show();
+      this.service.selected = new Director();
     }
-
-    getSelect() {
-        this.service.getStudents()
-            .subscribe(res => {
-                this.select_student = res as Student[];
-            });
-        this.service.getClubs()
-            .subscribe(res => {
-                this.select_club = res as Club[];
-            });
-    }
-
-    delete(_id: string) {
-        if (confirm('¿Estas seguro de eliminar este usuario?')) {
-            this.service.delete(_id)
-                .subscribe((res: any) => {
-                    this.get();
-                    this.AuthService.toast(res.message)
-                });
-        }
-    }
-
-    resetForm(form?: NgForm) {
-        if (form) {
-            form.reset();
-            $('#studentForm #edit').hide();
-            $('#studentForm #submit').show();
-            this.service.selected = new Director();
-        }
-    }
+  }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -9,99 +9,86 @@ import { Observable } from 'rxjs'
 declare var $: any;
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class AuthService {
-    selected: Director;
-    user: Director = new Director;
-    ip = window.location.hostname;
-    readonly URL_API = environment.baseUrl + "/api/directors";
-    readonly URL_students = environment.baseUrl + "/api/students";
-    readonly URL_CLUBS = environment.baseUrl + "/api/clubs";
+  selected: Director;
+  user: Director = new Director;
+  ip = window.location.hostname;
+  readonly URL_API = environment.baseUrl + "/api/directors";
+  readonly URL_students = environment.baseUrl + "/api/students";
+  readonly URL_CLUBS = environment.baseUrl + "/api/clubs";
 
-    constructor(private router: Router, private http: HttpClient) {
-        this.selected = new Director();
-    }
+  constructor(private router: Router, private http: HttpClient) {
+    this.selected = new Director();
+  }
 
-    authenticate(loginData) {
-        return this.http.post(this.URL_API + "/authenticate", loginData);
-    }
+  authenticate(loginData) {
+    return this.http.post(this.URL_API + "/authenticate", loginData);
+  }
 
-    toast(message, autohide?) {
-        var toast = document.getElementById("snackbar");
-        toast.className = "show";
-        toast.innerHTML = message;
-        setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 8000);
-    }
+  toast(message, autohide?) {
+    var toast = document.getElementById("snackbar");
+    toast.className = "show";
+    toast.innerHTML = message;
+    setTimeout(function () { toast.className = toast.className.replace("show", ""); }, 8000);
+  }
 
-    CurrentUser(): Observable<Director> {
-        return this.http.post<Director>(this.URL_API + "/me", {});
-    }
+  CurrentUser(): Observable<Director> {
+    return this.http.post<Director>(this.URL_API + "/me", {});
+  }
 
 
-    get gisAdmin() {
-        return this.user && this.user.role === "admin";
-    }
+  get gisAdmin() {
+    return this.user && this.user.role === "admin";
+  }
 
-    get gisDirector() {
-        return this.user && this.user.role === "director" || this.gisAdmin;
-    }
+  get gisDirector() {
+    return this.user && this.user.role === "director" || this.gisAdmin;
+  }
 
-    get gisSecretary() {
-        return this.user && this.user.role === "secretario" || this.gisDirector;
-    }
+  get gisSecretary() {
+    return this.user && this.user.role === "secretario" || this.gisDirector;
+  }
 
-    async isAdmin() {
-        return await this.user && this.user.role === "admin";
-    }
+  get iAm() {
+    return this.user.role;
+  }
 
-    async isDirector() {
-        return await this.user && this.user.role === "director" || this.isAdmin;
-    }
+  doLogout() {
+    this.router.navigate(["/"]);
+    window.localStorage.removeItem('token');
+    this.user = undefined;
+  }
 
-    async isSecretary() {
-        console.log(this.user.role)
-        return await this.user && this.user.role === "secretario" || this.isDirector;
-    }
+  doLogin(form?: NgForm) {
+    this.authenticate(form.value)
+      .subscribe((res: any) => {
+        if (res.success) {
+          this.user = res.user;
+          this.setToken(res.token);
+          this.router.navigate(["/clubs"]);
+        }
+        this.toast(res.message, true)
+      });
+  }
 
-    get iAm() {
-        return this.user.role;
-    }
+  setToken(token) {
+    window.localStorage.setItem('token', token);
+  }
 
-    doLogout() {
-        this.router.navigate(["/"]);
-        window.localStorage.removeItem('token');
-        this.user = undefined;
-    }
+  removeToken() {
+    window.localStorage.removeItem('token');
+  }
 
-    doLogin(form?: NgForm) {
-        this.authenticate(form.value)
-            .subscribe((res: any) => {
-                if (res.success) {
-                    this.user = res.user;
-                    this.setToken(res.token);
-                    this.router.navigate(["/clubs"]);
-                }
-                this.toast(res.message, true)
-            });
-    }
+  isLoggedIn() {
+    return !!window.localStorage.getItem('token');
+  }
 
-    setToken(token) {
-        window.localStorage.setItem('token', token);
-    }
-
-    removeToken() {
-        window.localStorage.removeItem('token');
-    }
-
-    isLoggedIn() {
-        return !!window.localStorage.getItem('token');
-    }
-
-    getToken() {
-        return window.localStorage.getItem('token');
-    }
+  getToken() {
+    return window.localStorage.getItem('token');
+  }
 
 }
 
